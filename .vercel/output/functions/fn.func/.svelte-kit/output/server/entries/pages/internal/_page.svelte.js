2b2d4fc449645fad9ea8f88ace4fe9cc085a4527
Ryan Morris <ryan.morris@centro.net>
2023-02-18 14:51:08 -0600

    fix(vercel): remove the vercel build from the repository

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/entries/pages/internal/_page.svelte.js
+++
@@ -1,5 +0,0 @@
-import { c as create_ssr_component } from '../../../chunks/index.js';
-const Page = create_ssr_component(($$result, $$props, $$bindings, slots) => {
-	return `<h1>Internal</h1>`;
-});
-export { Page as default };
--- .vercel/output/static/_app/immutable/chunks/3-29f56f38.js
+++
@@ -1,2 +0,0 @@
-import { default as t } from '../components/pages/internal/_page.svelte-27fbf52b.js';
-export { t as component };
