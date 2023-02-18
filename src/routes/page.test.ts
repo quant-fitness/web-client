16a3fbe1520a2bb6a85bb3e9a0719584a73aaa21
Ryan Morris <ryan.morris@centro.net>
2023-02-17 19:50:26 -0600

    Initial Commit

---
+++ src/routes/page.test.ts
@@ -0,0 +1,11 @@
+import Page from "./+page.svelte";
+import { describe, it, expect } from 'vitest';
+import { render, screen } from "@testing-library/svelte";
+import "@testing-library/jest-dom";
+
+describe("/", () => {
+  it("shows a heading", () => {
+    render(Page);
+    expect(screen.getByRole("heading", {name: "Welcome to SvelteKit"})).toBeInTheDocument();
+  });
+});
