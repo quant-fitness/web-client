57adb6eb5523abf5b2d13934936d9f971f50fa50
Ryan Morris <ryan.morris@centro.net>
2023-02-25 10:39:51 -0600

    feat(configuration): move the base URL for the API to an environment variable

--- src/routes/internal/health-checks/api/fetch-api-health.ts
+++ src/routes/internal/health-checks/api/fetch-api-health.ts
@@ -1,4 +1,5 @@
 import type { HealthStatus } from '../health-check-response';
+import { API_BASE_URL } from '$env/static/private';
 
 const checkApiHealthBody = async (response: Response): Promise<HealthStatus> =>
 	response
@@ -8,7 +9,7 @@ const checkApiHealthBody = async (response: Response): Promise<HealthStatus> =>
 
 export const fetchApiHealth = async ({ fetch }: any) => {
 	try {
-		const healthCheckResponse = await fetch('http://localhost:3000/api/v1/ping');
+		const healthCheckResponse = await fetch(`${API_BASE_URL}/api/v1/ping`);
 		return !healthCheckResponse.ok
 			? 'responded-with-an-error'
 			: checkApiHealthBody(healthCheckResponse);
